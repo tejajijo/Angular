8e837ac023a96f91892d260bfb52e456892dccb0
tejajijo <tejajijo11@gmail.com>
2024-05-29 14:42:32 +0100

    Commit on 29/05 at 2:42pm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'first-app_01-hello-world';
+  title = 'homes';
 }
